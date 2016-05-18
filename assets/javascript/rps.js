d42762d12dd4265775dc18ef39dee9446b6329a9
Shaina Veloz <shainaveloz@Shainas-MacBook-Air.local>
2016-05-18 17:28:37 -0400

    added player user function

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,73 +1,116 @@
 
 // reference to firebase
+var gameLocation = new Firebase("https://rps-game2.firebaseio.com/");
 var commentsRef = new Firebase("https://rps-game2.firebaseio.com/Chat");
 
-game = [{
-	"choices" : ["rock","paper", "scissors"],
-	"player" : ["1", "2"]
-}]
 
 function go() {
-  var name = prompt('Name?', 'Guest');
-  // Consider adding '/<unique id>' if you have multiple games.
-  var gameData = new Firebase("https://rps-game2.firebaseio.com/Players");
-  assignPlayerNumberAndPlayGame(userId, gameRef);
+var name = prompt('Name?', 'Guest');
+ 
+var gameData = gameLocation;
+assignPlayerNumberAndPlayGame(name, gameData);
 };
 
-gameData.on("value", function(snapshot) {
+var NUM_PLAYERS = 2;
+var PLAYERS_LOCATION = 'player_list';
+var PLAYER_DATA_LOCATION = 'player_data';
 
-	var wins = 0;
+function playGame(myPlayerNumber, name, justJoinedGame, gameData) {
+  var playerDataRef = gameRef.child(PLAYER_DATA_LOCATION).child(myPlayerNumber);
+  alert('You are player number ' + myPlayerNumber + 
+      '.  Your data will be located at ' + playerDataRef.toString());
 
-	var loses = 0;
+  if (justJoinedGame) {
+    alert('Doing first-time initialization of data.');
+    playerDataRef.set({
+    	name: name, 
+    	state: 'game state'
+    });
+  }
+}
 
-	var ties = 0;
+function assignPlayerNumberAndPlayGame(name, gameData) {
+  var playerListRef = gameData.child(PLAYERS_LOCATION);
+  var myPlayerNumber, alreadyInGame = false;
 
-	if (snapshot.child("rock").exists() && snapshot.child("rock").exists()){
-		ties += 1;
+ playerListRef.transaction(function(playerList) {
+ 	if (playerList === null) {
+      playerList = [];
+    }
+	for (var i = 0; i < playerList.length; i++) {
+	    if (playerList[i] === name) {
+	  	    alreadyInGame = true;
+	        myPlayerNumber = i; 
+	        return;
+    	}
 	}
-	 else if (snapshot.child("rock").exists() && snapshot.child("paper").exists()){
-			loses += 1;
-		}
-		else if (snapshot.child("rock").exists() && snapshot.child("scissors").exists()) {
-			wins += 1;
-			alert("Congrats, YOU WIN absoulutely nothing!");
-		}
-		else if (snapshot.child("paper").exists() && snapshot.child("paper").exists()){
-			ties += 1;
-		}
-		else if (snapshot.child("paper").exists() && snapshot.child("scissors").exists()){
-			loses += 1;
-		}
-		else if (snapshot.child("paper").exists() && snapshot.child("rock").exists()){
-			wins += 1;
-			alert("Congrats, YOU WIN absoulutely nothing!");
-		}
+	if (i < NUM_PLAYERS) {
+	 	    playerList[i] = name;  
+    myPlayerNumber = i; 
+    return playerList;
+    }
+    myPlayerNumber = null;
+}, function (error, committed) {
+		if (committed || alreadyInGame) {
+      		playGame(myPlayerNumber, name, !alreadyInGame, gameData);
+    	} else {
+      	alert('Game is full.');
+    	}
+  	});
+}
+// gameData.on("value", function(snapshot) {
 
-		else if (snapshot.child("scissors").exists() && snapshot.child("scissors").exists()){
-			ties += 1;
-		}
-		else if (snapshot.child("scissors").exists() && snapshot.child("rock").exists()){
-			loses += 1;
-		}
-		else if (snapshot.child("scissors").exists() && snapshot.child("paper").exists()){
-			wins += 1;
-			alert("Congrats, YOU WIN absoulutely nothing!");
-		}
+// 	var wins = 0;
+
+// 	var loses = 0;
+
+// 	var ties = 0;
+
+// 	if (snapshot.child("rock").exists() && snapshot.child("rock").exists()){
+// 		ties += 1;
+// 	}
+// 	 else if (snapshot.child("rock").exists() && snapshot.child("paper").exists()){
+// 			loses += 1;
+// 		}
+// 		else if (snapshot.child("rock").exists() && snapshot.child("scissors").exists()) {
+// 			wins += 1;
+// 			alert("Congrats, YOU WIN absoulutely nothing!");
+// 		}
+// 		else if (snapshot.child("paper").exists() && snapshot.child("paper").exists()){
+// 			ties += 1;
+// 		}
+// 		else if (snapshot.child("paper").exists() && snapshot.child("scissors").exists()){
+// 			loses += 1;
+// 		}
+// 		else if (snapshot.child("paper").exists() && snapshot.child("rock").exists()){
+// 			wins += 1;
+// 			alert("Congrats, YOU WIN absoulutely nothing!");
+// 		}
+
+// 		else if (snapshot.child("scissors").exists() && snapshot.child("scissors").exists()){
+// 			ties += 1;
+// 		}
+// 		else if (snapshot.child("scissors").exists() && snapshot.child("rock").exists()){
+// 			loses += 1;
+// 		}
+// 		else if (snapshot.child("scissors").exists() && snapshot.child("paper").exists()){
+// 			wins += 1;
+// 			alert("Congrats, YOU WIN absoulutely nothing!");
+// 		}
 	
 	
-			console.log("wins = "  + wins);
-			console.log("Loses = "  + loses);
-			console.log("Ties = "  + ties);
+// 			console.log("wins = "  + wins);
+// 			console.log("Loses = "  + loses);
+// 			console.log("Ties = "  + ties);
 
-			$('#loses').html(loses);
-			$('#wins').html(wins);
-			$('#ties').html(ties);
-})
+// 			$('#loses').html(loses);
+// 			$('#wins').html(wins);
+// 			$('#ties').html(ties);
+// })
 
 
   // REGISTER DOM ELEMENTS
 commentsRef.on("value", function(snapshot){
-	var commentField = $('#comment-input').val();
 	var nameField = $('#nameInput').val();
 	var messageList = $('#messages').val();
 })
@@ -97,9 +140,9 @@ $(".submit").on("click", function() {
 	var currentWins = wins;
 	var currentLosses = loses;
 	var currentTies = ties;
-	var comment = commentField.val();
+	var comment = $('#comment-input').val();
 
-		gameData.push({
+		gameLocation.push({
 		name: name,
 		currentWins : currentWins,
 		currentLosses : currentLosses,
@@ -110,28 +153,18 @@ $(".submit").on("click", function() {
 		name:name, 
 		text:comment
 	});
-      	commentField.val('');
+      	comment.val('');
 
 		return false;
 });
 // gameData.orderByChild("rock").equalTo(rock).on("child_added", function(snapshot) {
 //   console.log(snapshot.key());
 // });
-
-// on click button events
-
-
-
-  // LISTEN FOR KEYPRESS EVENT
-  
-  
-
-  // Add a callback that is triggered for each chat message.
  
 
 
 $(".submitInfo").on("click", function() {
-	gameData.set({
+	gameLocation.set({
 			newPlayer: addPlayer,
 			newChoice: addChoice,
 			wins: wins,
